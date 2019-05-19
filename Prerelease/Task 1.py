95c8e81872ec75edd3c71090e05c9b1f5e9b2c2e
Charles Ji <39901664+CharlieGai@users.noreply.github.com>
2019-05-19 15:49:11 +0800

    Final Edition!!

--- Prerelease/Task 1.py
+++ Prerelease/Task 1.py
@@ -1,22 +1,25 @@
-# Task1
-n = 0
-item_no = [0] * n
-description = [""] * n
-reserve_price = [0] * n
-num_of_bids = [0] * n
+number_of_items = int(input("How many items are in the auction? "))
 
-while n < 10:
-    print("There should be at least 10 items")
-    n = int(input("Number of items? ="))
+while number_of_items < 10:
+    print('There must be a minimum of 10 items in the auction.\nPlease enter another number.')
+    number_of_items = int(input("How many items are in the auction? "))
 
-for i in range(0, n):
-    print("Input the data for item", i + 1)
-    item_no[i] = int(input("ItemNo of item ="))
+item_number = [0] * number_of_items
+item_list = [''] * number_of_items
+description_list = [''] * number_of_items
+reserve_price_list = [0] * number_of_items
+number_of_bids = [0] * number_of_items
 
-    for j in range(0, i):
-        while item_no[i] == item_no[j]:
-            print("The item number must be unique !")
-            item_no[i] = int(input("ItemNo of item ="))
-            j = 0
-    description[i] = input("Description of item =")
-    reserve_price[i] = int(input("Reverse price of item ="))
+for i in range(number_of_items):
+    item_number[i] = i
+    item_list[i] = input('Please enter an item: ')
+    description_list[i] = input('Please enter a description: ')
+    reserve_price_list[i] = int(input('Please enter a reserve price: '))
+
+# test code
+print(item_list, len(item_list))
+print(item_number, len(item_number))
+print(description_list, len(description_list))
+print(reserve_price_list, len(reserve_price_list))
+print(number_of_bids, len(number_of_bids))
+
